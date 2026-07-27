436ee27ee44b44f533bce9fcd4c9896e716fdce4
Marc Marcos <marcmarcosmadruga@gmail.com>
2026-07-27 11:27:19 +0800

    provisional commit

--- src/main/scala/yunsuan/vector/VectorALU/VCrypto.scala
+++ src/main/scala/yunsuan/vector/VectorALU/VCrypto.scala
@@ -4,6 +4,7 @@ package yunsuan.vector.VectorALU
 import chisel3._
 import chisel3.util._
 import yunsuan.vector.v2.Crypto.VClmul
+import yunsuan.vector.v2.Crypto.VAes
 
 class VCrypto extends Module {
   val io = IO(new Bundle {
@@ -12,6 +13,7 @@ class VCrypto extends Module {
   })
 
   val isHigh = io.in.bits.opcode.isVclmulh
+  val isAesZero = io.in.bits.opcode.isVaesz
 
   val hi_hi = Wire(UInt(64.W))
   val hi_lo = Wire(UInt(64.W))
@@ -34,7 +36,19 @@ class VCrypto extends Module {
 
   val result = Cat(Mux(isHigh, hi_hi, hi_lo), Mux(isHigh, lo_hi, lo_lo))
 
-  val stage1Result = RegEnable(result, io.in.valid)
+  // VAESZ is the first AES encryption round without a round key.
+  val aes = Module(new VAes)
+  aes.in.valid := io.in.valid
+  aes.in.bits.op.em := false.B
+  aes.in.bits.op.ef := true.B
+  aes.in.bits.op.dm := false.B
+  aes.in.bits.op.df := false.B
+  aes.in.bits.vs3 := io.in.bits.old_vd
+  aes.in.bits.vs2 := 0.U
+
+  val cryptoResult = Mux(isAesZero, aes.out.vd, result)
+
+  val stage1Result = RegEnable(cryptoResult, io.in.valid)
   val stage1Valid = RegNext(io.in.valid, false.B)
 
   io.out.bits.vd    := RegEnable(stage1Result, stage1Valid)
@@ -44,14 +58,16 @@ class VCrypto extends Module {
 
 object VCrypto {
   object Opcode {
-    val vclmul  = 0.U(1.W)
-    val vclmulh = 1.U(1.W)
+    val vclmul  = 0.U(3.W)
+    val vclmulh = 1.U(3.W)
+    val vaesz   = 2.U(3.W)
   }
 
   class Opcode extends Bundle {
-    val op = UInt(1.W)
+    val op = UInt(3.W)
 
     def isVclmulh: Bool = op === Opcode.vclmulh
+    def isVaesz: Bool = op === Opcode.vaesz
   }
 
   class In extends Bundle {
